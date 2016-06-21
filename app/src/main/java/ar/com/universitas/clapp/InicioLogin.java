66b9fd60031c5b3067c197491c98063aa6a80af9
Julian <julian.varela90@gmail.com>
2016-06-20 21:05:33 -0300

    Prueba uno corrigiendo iniciologin y mainactivity

--- app/src/main/java/ar/com/universitas/clapp/InicioLogin.java
+++ app/src/main/java/ar/com/universitas/clapp/InicioLogin.java
@@ -3,10 +3,7 @@ package ar.com.universitas.clapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 /**
--- app/src/main/java/ar/com/universitas/clapp/MainActivity.java
+++ app/src/main/java/ar/com/universitas/clapp/MainActivity.java
@@ -1,11 +1,10 @@
 package ar.com.universitas.clapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnarmilist;
