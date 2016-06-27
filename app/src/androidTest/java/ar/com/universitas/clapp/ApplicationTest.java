d9a235459956d7d3a9223f8e6e78100f12e3d4a0
Chinuxs <august.cesar.romer@gmail.com>
2016-06-27 20:01:10 -0300

    -> Chinuxs: Gente agregeue un comentario y lo comit

--- app/src/androidTest/java/ar/com/universitas/clapp/ApplicationTest.java
+++ app/src/androidTest/java/ar/com/universitas/clapp/ApplicationTest.java
@@ -7,6 +7,8 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    /* ----------agregue un comentatio -chino-------------*/
     public ApplicationTest() {
         super(Application.class);
     }
