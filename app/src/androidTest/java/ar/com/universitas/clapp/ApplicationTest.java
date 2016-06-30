59a791f9f936a18f67812eee1f3c252714dfaffc
Julian <julian.varela90@gmail.com>
2016-06-30 19:26:16 -0300

    Hice cambio en clase aplication test

--- app/src/androidTest/java/ar/com/universitas/clapp/ApplicationTest.java
+++ app/src/androidTest/java/ar/com/universitas/clapp/ApplicationTest.java
@@ -12,4 +12,6 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    /*--------enzo gato migranico-------*/
 }
