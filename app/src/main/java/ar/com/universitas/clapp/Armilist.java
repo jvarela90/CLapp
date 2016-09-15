7b56fb71a458dc4abc944fa79a767e5de1d3a06a
Julian <julian.varela90@gmail.com>
2016-09-14 22:23:18 -0300

    1de Septiembre 4.0

--- app/src/main/java/ar/com/universitas/clapp/Armilist.java
+++ app/src/main/java/ar/com/universitas/clapp/Armilist.java
@@ -1,18 +1,179 @@
 package ar.com.universitas.clapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Armilist extends Activity {
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> empresaList;
+
+
+    // url to get all products list
+    private static String url_all_usuarios = "http://clappuniv.esy.es/clapp/get_all_usuarios.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "usuario";
+    private static final String TAG_ID = "idusuario";
+    private static final String TAG_NOMBRE = "username";
+
+    // products JSONArray
+    JSONArray products = null;
+
+    ListView lista;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.armilist);
+
+        // Hashmap para el ListView
+        empresaList = new ArrayList<HashMap<String, String>>();
+
+        // Cargar los productos en el Background Thread
+        new LoadAllProducts().execute();
+        lista = (ListView) findViewById(R.id.listAllProducts);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }//fin onCreate
+
+
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+
+        /**
+         * Antes de empezar el background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Armilist.this);
+            pDialog.setMessage("Cargando usuarios. Por favor espere...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * obteniendo todos los productos
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List params = new ArrayList();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_usuarios, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    products = json.getJSONArray(TAG_PRODUCTS);
+
+                    // looping through All Products
+                    //Log.i("ramiro", "produtos.length" + products.length());
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject c = products.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NOMBRE);
+
+                        // creating new HashMap
+                        HashMap map = new HashMap();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_ID, id);
+                        map.put(TAG_NOMBRE, name);
+
+                        empresaList.add(map);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed JSON data into ListView
+                     * */
+                    ListAdapter adapter = new SimpleAdapter(
+                            Armilist.this,
+                            empresaList,
+                            R.layout.single_post,
+                            new String[] {
+                                    TAG_ID,
+                                    TAG_NOMBRE,
+                            },
+                            new int[] {
+                                    R.id.single_post_tv_id,
+                                    R.id.single_post_tv_nombre,
+                            });
+                    // updating listview
+                    //setListAdapter(adapter);
+                    lista.setAdapter(adapter);
+                }
+            });
+        }
+    }
+}
+
+
+/*
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Created by Julian on 12/06/2016.
- *
- *
- *2
- * ESTO ES PARA PROBAR EL COMMITEO para Cesar
- */
+
 public class Armilist extends Activity {
 
     Button btnvolver;
@@ -33,3 +194,4 @@ public class Armilist extends Activity {
 
 
 }
+*/
