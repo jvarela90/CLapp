e52102ff3b6dcaf30e5355a23b090b81dad3d809
Julian <julian.varela90@gmail.com>
2016-09-20 19:23:07 -0300

    cambio 20/09

--- app/src/androidTest/java/ar/com/universitas/clapp/ApplicationTest.java
+++ app/src/androidTest/java/ar/com/universitas/clapp/ApplicationTest.java
@@ -8,7 +8,7 @@ import android.test.ApplicationTestCase;
  *
  *
  *
- * ESTO ES PARA PROBAR EL COMMITEO para Cesar
+ * ESTO ES PARA PROBAR EL COMMITEO para Cesar y de tu hermana
  */
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
