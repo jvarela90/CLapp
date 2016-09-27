2f61832db74cf933714b184c01846570f3507c50
Julian <julian.varela90@gmail.com>
2016-09-27 20:19:23 -0300

    27/09 1.0

--- app/src/main/java/ar/com/universitas/clapp/ListViewCheckBoxesActivity.java
+++ app/src/main/java/ar/com/universitas/clapp/ListViewCheckBoxesActivity.java
@@ -1,23 +1,23 @@
 package ar.com.universitas.clapp;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
+
+import java.util.ArrayList;
 
 public class ListViewCheckboxesActivity extends Activity {
 
